366269914d89f3db42bdd669836a1af57e85003d
evgen <fiend.jackson@gmail.com>
2022-08-12 17:47:22 +0300

    nowa

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -25,10 +25,37 @@ const swiper3 = new Swiper('.rails__slider.swiper', {
   },
 });
 
+const thumbsSwiper4 = new Swiper(".product__thumbs.swiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+const swiper4 = new Swiper('.product__slider.swiper', {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.product__slider .swiper-button-next',
+    prevEl: '.product__slider .swiper-button-prev',
+  },
+  thumbs: {
+    swiper: thumbsSwiper4
+  }
+
+});
+
+
+
 const drops = document.querySelectorAll('.drop__btn');
 
 drops.forEach(drop => drop.addEventListener('click', () => {
-    drop.classList.toggle('opened');
-    const content = drop.nextSibling;
-    content.classList.toggle('opened');
-  }));
+  drop.classList.toggle('opened');
+  const content = drop.nextSibling;
+  content.classList.toggle('opened');
+}));
+
+const accordeonToggle = document.querySelectorAll('.accordeon__title');
+
+accordeonToggle.forEach(acc => acc.addEventListener('click', () => {
+  acc.parentElement.classList.toggle('opened');
+}));
